bc1a8b4007a5286888a328c0fa4859cf1ccd9ab5
ModzabazeR <moddaeng.pps@gmail.com>
2024-02-11 16:45:29 +0700

    update

--- Assignment2_65050581_65050777.java
+++ Assignment2_65050581_65050777.java
@@ -11,53 +11,6 @@ import java.util.Queue;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 
-class MyColor extends Color {
-
-	public MyColor(int r, int g, int b) {
-		super(r, g, b);
-	}
-
-	public static final Color PLACEHOLDER = Color.decode("#f5f5f5");
-	public static final Color OIL = Color.decode("#291C16");
-	public static final Color TACAO = Color.decode("#ECBA88");
-	public static final Color EERIE_BLACK = Color.decode("#1B191A");
-	public static final Color DARK_TACAO = Color.decode("#C2855D");
-	public static final Color EERIE = Color.decode("#252525");
-	public static final Color STRAW_HAT = Color.decode("#F2B862");
-	public static final Color STRAW_HAT_SHADOW = Color.decode("#7C4E1B");
-	public static final Color HEADDAND = Color.decode("#DC002E");
-	public static final Color HEADDAND_SHADOW = Color.decode("#8C0324");
-	public static final Color HAIR_LUFFY = Color.decode("#1E1E1E");
-	public static final Color SKIN_LUFFY_COLOR = Color.decode("#F1C6A3");
-	public static final Color SKIN_LUFFY_SHADOW = Color.decode("#CC9167");
-	public static final Color SHIRT_LUFFY = Color.decode("#9D2024");
-	public static final Color SHIRT_LUFFY_SHADOW = Color.decode("#621D16");
-	public static final Color SHIRT_LUFFY_CHILD = Color.decode("#ECE7E1");
-	public static final Color SHIRT_LUFFY_CHILD_SHADOW = Color.decode("#A1A2A6");
-	public static final Color BLUE_COLOR = Color.decode("#21386C");
-	public static final Color BLUE_SHADOW = Color.decode("#242128");
-	public static final Color RED_COLOR = Color.decode("#A93D3B");
-	public static final Color RED_SHADOW = Color.decode("#6B1C21");
-	public static final Color SKIN_LUFFY_CHILD = Color.decode("#EFC8A1");
-	public static final Color SKIN_LUFFY_CHILD_SHADOW = Color.decode("#BA7F5E");
-	
-	public static final Color TROUSERS_LUFFY = Color.decode("#2B5F91");
-	public static final Color TROUSERS_LUFFY_SHADOW = Color.decode("#1A4361");
-	public static final Color PANTSEDGE_LUFFY  = Color.decode("#B5D5D4");
-	public static final Color SHOE = Color.decode("#918976");
-	public static final Color SHOEEDGE = Color.decode("#29201B");
-
-	public static final Color BANANA = Color.decode("#FFCE00");
-	public static final Color SUN = Color.decode("#FF0409");
-
-	public static final Color LINE_ON_LOGO = Color.decode("#62C3F8");
-	public static final Color LETTER = Color.decode("#4489C7");
-	public static final Color UNDERSCORE_LOGO = Color.decode("#2E63A4");
-
-	public static final Color CRIMSON_RED = Color.decode("#D70000");
-	public static final Color OAKUM = Color.decode("#AF6528");
-}
-
 public class Assignment2_65050581_65050777 extends JPanel implements Runnable {
     private boolean doDrawTitle = true;
     private boolean doDrawTitleHat = false;
